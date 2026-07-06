d8a17fc816f7176e6ba01831695efc13ae90d73b
kuna9966 <chillichikki3@example.com>
2026-07-06 06:20:37 +0530

    Google Tag updated in every page

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -156,6 +156,14 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     scripts: [
       {
+        src: "https://www.googletagmanager.com/gtag/js?id=G-137R4392R4",
+        async: true,
+      },
+      {
+        children:
+          "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-137R4392R4');",
+      },
+      {
         type: "application/ld+json",
         children: JSON.stringify({
           "@context": "https://schema.org",
